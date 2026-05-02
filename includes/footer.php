f3e56b76f880734797b3e01f4b6327373dc27e82
OUSSAMA <oussamacharef71@gmail.com>
2026-05-02 20:15:47 +0100

    Add site footer markup

---
+++ includes/footer.php
@@ -0,0 +1,64 @@
+<!-- Footer Section -->
+<footer class="site_footer">
+    <div class="footer_top">
+        
+        <!-- About Column -->
+        <div class="footer_about">
+            <a href="index.php" class="logo footer_logo">
+                <div class="logo_icon"><i class="fa-solid fa-compass"></i></div>
+                <span class="logo_text">Tangier <span class="highlight">Vibes</span></span>
+            </a>
+            <p class="footer_desc">
+                Your ultimate guide to discovering the magic, culture,
+                and coastal beauty of Tangier, Morocco.
+            </p>
+        </div>
+
+        <!-- Quick Links Column -->
+        <div class="footer_col">
+            <h4 class="footer_col_title">Quick Links</h4>
+            <ul class="footer_links">
+                <li><a href="index.php" class="footer_link">Home</a></li>
+                <li><a href="#" class="footer_link">Top Places</a></li>
+                <li><a href="explore.php" class="footer_link">Explore</a></li>
+                <li><a href="#" class="footer_link">Favorites</a></li>
+            </ul>
+        </div>
+   
+        <!-- Categories Column -->
+        <div class="footer_col">
+            <h4 class="footer_col_title">Categories</h4>
+            <ul class="footer_links">
+                <li><a href="#" class="footer_link">Hotels</a></li>
+                <li><a href="#" class="footer_link">Restaurants</a></li>
+                <li><a href="#" class="footer_link">Cafes</a></li>
+                <li><a href="#" class="footer_link">Beaches</a></li>
+                <li><a href="#" class="footer_link">Parks</a></li>
+                <li><a href="#" class="footer_link">Museum</a></li>
+            </ul>
+        </div>
+
+        <!-- Social Media Column -->
+        <div class="footer_col">
+            <h4 class="footer_col_title">Follow Us</h4>
+            <div class="footer_socials">
+                <a href="#" class="social_icon" aria-label="Facebook">
+                    <i class="fa-brands fa-facebook-f"></i>
+                </a>
+                <a href="#" class="social_icon" aria-label="Instagram">
+                    <i class="fa-brands fa-instagram"></i>
+                </a>
+                <a href="#" class="social_icon" aria-label="Twitter">
+                    <i class="fa-brands fa-x-twitter"></i>
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <!-- Bottom Copyright -->
+    <div class="footer_bottom">
+        <p class="footer_copy">
+            © 2026 TangierVibes. All rights reserved.
+        </p>
+    </div>
+</footer>
