492a8afe237fd828c7b66f7844cde84bfbbc1701
OUSSAMA <oussamacharef71@gmail.com>
2026-05-12 11:54:01 +0200

    Update header navigation tweaks

--- includes/header.php
+++ includes/header.php
@@ -34,14 +34,14 @@ $current_page = basename($_SERVER['PHP_SELF']);
 
                 <!-- nav links desktop -->
             <ul class="nav_links desktop_only">
-                <li><a href="<?= ($current_page == 'index.php') ? 'index.php' : '../index.php' ?>" class="nav_link <?php echo ($current_page == 'index.php') ? 'active' : ''; ?>">Home</a></li>
-                <li><a href="#" class="nav_link <?php echo ($current_page == 'top_places.php') ? 'active' : ''; ?>">Top Places</a></li>
-                <li><a href="<?= ($current_page == 'index.php') ? 'pages/explore.php' : 'explore.php' ?>" class="nav_link <?php echo ($current_page == 'explore.php') ? 'active' : ''; ?>">Explore</a></li>
-                <li><a href="#" class="nav_link <?php echo ($current_page == 'favorites.php') ? 'active' : ''; ?>"><i class="fa-regular fa-heart"></i> Favorites</a></li>
+                <li><a href="../index.php" class="nav_link <?php echo ($current_page == 'index.php') ? 'active' : ''; ?>">Home</a></li>
+                <li><a href="#" class="nav_link">Top Places</a></li>
+                <li><a href="explore.php" class="nav_link <?php echo ($current_page == 'explore.php') ? 'active' : ''; ?>">Explore</a></li>
+                <li><a href="#" class="nav_link"><i class="fa-regular fa-heart"></i> Favorites</a></li>
             </ul>
 
             <div class="header_search_container desktop_only">
-                <form class="header_search_form" action="<?= ($current_page == 'index.php') ? 'pages/explore.php' : 'explore.php' ?>" method="GET">
+                <form class="header_search_form" action="explore.php" method="GET">
                     <i class="fa-solid fa-magnifying-glass search_icon"></i>
                     <input type="text" name="search" placeholder="Search places..." class="header_search_input" value="<?= isset($_GET['search']) ? htmlspecialchars($_GET['search']) : '' ?>" >
                 </form>
@@ -64,8 +64,8 @@ $current_page = basename($_SERVER['PHP_SELF']);
                                     <span class="user_role">Administrator</span>
                                 </div>
                                 <hr>
-                                <a href="<?= ($current_page == 'index.php') ? 'pages/dashboard.php' : 'dashboard.php' ?>" class="dropdown_item"><i class="fa-solid fa-gauge"></i> Dashboard</a>
-                                <a href="<?= ($current_page == 'index.php') ? 'pages/logout.php' : 'logout.php' ?>" class="dropdown_item logout"><i class="fa-solid fa-right-from-bracket"></i> Logout</a>
+                                <a href="pages/dashboard.php" class="dropdown_item"><i class="fa-solid fa-gauge"></i> Dashboard</a>
+                                <a href="pages/logout.php" class="dropdown_item logout"><i class="fa-solid fa-right-from-bracket"></i> Logout</a>
                             </div>
                         </div>
 
@@ -77,7 +77,7 @@ $current_page = basename($_SERVER['PHP_SELF']);
                         <div class="welcome_group">
                             <span class="welcome_text">Welcome, <strong><?= htmlspecialchars($_SESSION['full_name']); ?></strong></span>
                         </div>
-                        <a href="<?= ($current_page == 'index.php') ? 'pages/logout.php' : 'logout.php' ?>" class="logout_btn">Logout</a>
+                        <a href="pages/logout.php" class="logout_btn">Logout</a>
                         <?php endif; ?>
                         
                     <?php else: ?>
@@ -110,7 +110,7 @@ $current_page = basename($_SERVER['PHP_SELF']);
             </div>
 
             <div class="mobile_search_in_menu">
-                <form action="<?= ($current_page == 'index.php') ? 'pages/explore.php' : 'explore.php' ?>" method="GET">
+                <form action="explore.php" method="GET">
                     <i class="fa-solid fa-magnifying-glass"></i>
                     <input type="text" name="search" placeholder="Search..." value="<?= isset($_GET['search']) ? htmlspecialchars($_GET['search']) : '' ?>">
                 </form>
@@ -118,9 +118,9 @@ $current_page = basename($_SERVER['PHP_SELF']);
 
             <ul class="mobile_menu_links">
                 <li><a href="../index.php" class="nav_link <?php echo ($current_page == 'index.php') ? 'active' : ''; ?>"><i class="fa-solid fa-house "></i> Home</a></li>
-                <li><a href="#" class="nav_link <?php echo ($current_page == 'topplaces.php') ? 'active' : ''; ?>"><i class="fa-solid fa-house "></i> Top Places</a></li>
+                <li><a href="#" class="nav_link"><i class="fa-solid fa-house "></i> Top Places</a></li>
                 <li><a href="../pages/explore.php" class="nav_link <?php echo ($current_page == 'explore.php') ? 'active' : ''; ?>"><i class="fa-solid fa-compass"></i> Explore</a></li>
-                <li><a href="#" class="nav_link <?php echo ($current_page == 'favorites.php') ? 'active' : ''; ?>"><i class="fa-solid fa-compass"></i> Favorites</a></li>
+                <li><a href="#" class="nav_link"><i class="fa-solid fa-compass"></i> Favorites</a></li>
                 
                 <hr class="menu_divider">
 
