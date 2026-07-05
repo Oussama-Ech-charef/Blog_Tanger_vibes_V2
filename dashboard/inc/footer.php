d93d2fde31f33a744770e4a03ede5005b310ca75
OUSSAMA <oussamacharef71@gmail.com>
2026-07-06 00:43:38 +0100

    Support extra dashboard page scripts

--- dashboard/inc/footer.php
+++ dashboard/inc/footer.php
@@ -2,7 +2,9 @@
     </div><!-- /.dashboard_main -->
 </div><!-- /.dashboard_layout -->
 
-
 <script src="../assets/js/dashboard.js"></script>
+<?php if (!empty($extra_scripts)): foreach ($extra_scripts as $script): ?>
+<script src="<?= htmlspecialchars($script) ?>"></script>
+<?php endforeach; endif; ?>
 </body>
 </html>
